8af6f237938b484cb7d4d7990f7410e0b8dbf2fa
Your Name <you@example.com>
2025-11-12 12:07:18 +0300

    Обновление

--- src/app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from 'next';
-
-import { getRobotsMetadata } from '@/lib/robots';
-
-export const dynamic = 'force-static';
-
-export default async function robots(): Promise<MetadataRoute.Robots> {
-  return getRobotsMetadata();
-}
