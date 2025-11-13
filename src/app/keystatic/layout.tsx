190a9f73279c4cf02c0a544297895c0a9a3e06b7
Your Name <you@example.com>
2025-11-13 11:23:48 +0300

    Обновление

--- src/app/keystatic/layout.tsx
+++ src/app/keystatic/layout.tsx
@@ -1,3 +1,8 @@
 // src/app/keystatic/layout.tsx
+import type { ReactElement } from "react";
+
 import KeystaticApp from "./keystatic";
-export default function Layout() { return <KeystaticApp />; }
+
+export default function Layout(): ReactElement {
+  return <KeystaticApp />;
+}
