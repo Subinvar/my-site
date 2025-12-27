218eb7c34dfa52c361980fec9d24bf2f7957046c
Subinvar <subinvariants@gmail.com>
2025-12-28 01:38:50 +0300

    Обновление

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,14 +2,14 @@ import { Montserrat, Open_Sans } from "next/font/google";
 
 export const headingFont = Montserrat({
   subsets: ["latin", "cyrillic"],
-  weight: ["500", "600", "700"],
+  weight: ["500", "600", "700"],   // можно ужать до ["600","700"], если 500 реально нигде не нужен
   display: "swap",
-  variable: "--font-heading",
+  variable: "--font-heading-var",
 });
 
 export const bodyFont = Open_Sans({
   subsets: ["latin", "cyrillic"],
-  weight: ["400", "500", "600"],
+  weight: ["400", "500", "600"],   // можно ужать до ["400","600"]
   display: "swap",
-  variable: "--font-body",
+  variable: "--font-body-var",
 });
